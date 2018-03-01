1181faadf8fe0557ab9b65f0e4aa6b800c346810
Kat Dobra <broun.kat@gmail.com>
2018-03-01 11:07:34 +0200

    changed index.js to load static files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send({ hi: 'there' });
+app.use(express.static('./client'));
+
+app.get('*',function (req,res) {
+  res.sendFile('./index.html');
 });
 
-var PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(5000);
